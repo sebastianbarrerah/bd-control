6746a4ecc882d33c66f11e6d0a5bc50dbf598ee5
Sebastian barrera <sebasbh2008@hotmail.com>
2023-11-08 18:34:28 -0500

    new

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.defaults();
+const router = jsonServer.router(`${__dirname}/db.json`);
 const middlewares = jsonServer.defaults();
-const port = porcess.env.PORT || 3001;
 
-server.use(middlewares);
+const port = process.env.PORT || 3000;
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
 server.use(router);
+server.use(middlewares);
 
-server.listen(port);
+server.listen(port, () => {
+console.log("JSON Server is running");
+});
